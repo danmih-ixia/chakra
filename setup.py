36d9a295e83f0c60041d803b4846ad7a799de53f
AlexandruAntonescuKeysight <alexandru.antonescu@keysight.com>
2024-06-12 05:14:32 +0300

    pip install version from last tag

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from distutils.command.build import build
 
 from setuptools import setup
-
+import os
 
 class build_grpc(build):
     sub_commands = [("build_grpc", None)] + build.sub_commands
 
 
-setup(cmdclass={"build": build_grpc})
+version_file = os.path.join(os.path.dirname(__file__), "./", "version.py")
+with open(version_file) as f:
+    exec(f.read())
+
+setup (
+    cmdclass={
+        "build": build_grpc
+    },
+    version = __version__
+)
